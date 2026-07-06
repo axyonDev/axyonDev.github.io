aa03677a58f829f427b571d5f1ae01f7f443abe7
Axyon <axyon.dev@gmail.com>
2026-07-06 20:08:31 +0300

    54

--- oyun/sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'axyon-factory-v3-0';
-const ASSETS = ['./','./index.html','./css/style.css','./manifest.json',
-  './src/core/numbers.js','./data/config.js','./src/core/economy.js','./src/core/quests.js',
-  './src/services/save-service.js','./src/ui/toast.js','./src/canvas/factory-canvas.js','./src/ui/ui.js','./src/main.js'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
